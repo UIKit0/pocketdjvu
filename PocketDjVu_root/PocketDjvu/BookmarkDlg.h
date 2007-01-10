630924d28b937c89808ecbc0e48de8a9e0d85fb1
Igor.Solovyov <Igor.Solovyov@84cd470b-3125-0410-acc3-039690e87181>
2007-01-10 21:53:53 +0000

    The way how to resize the dialog if SIP was selected was investigated.

--- PocketDjVu_root/PocketDjvu/BookmarkDlg.h
+++ PocketDjVu_root/PocketDjvu/BookmarkDlg.h
@@ -27,8 +27,9 @@ BEGIN_DLGRESIZE_MAP(CAboutDlg)
 END_DLGRESIZE_MAP()
 
 BEGIN_MSG_MAP(CBookmarkDlg)
-	MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
-	COMMAND_ID_HANDLER(IDOK, OnClickedOK)
+	MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)	
+  MESSAGE_HANDLER(WM_WININICHANGE, OnWininiChange)
+  COMMAND_ID_HANDLER(IDOK, OnClickedOK)
 	CHAIN_MSG_MAP(Base)
 END_MSG_MAP()
 
@@ -37,8 +38,9 @@ END_MSG_MAP()
 //  LRESULT CommandHandler(WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled);
 //  LRESULT NotifyHandler(int idCtrl, LPNMHDR pnmh, BOOL& bHandled);
 
-  LRESULT OnInitDialog( UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled );
   LRESULT OnClickedOK( WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled );
+  LRESULT OnInitDialog( UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled );
+  LRESULT OnWininiChange( UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled );
 
 // DATA:
 private:
