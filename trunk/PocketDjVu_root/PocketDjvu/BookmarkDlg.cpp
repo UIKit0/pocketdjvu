5db6d7925c89f45f7b219834492396250aa27333
Igor.Solovyov <Igor.Solovyov@84cd470b-3125-0410-acc3-039690e87181>
2007-01-10 20:25:59 +0000

    CBookmarkDlg was added.

---
+++ trunk/PocketDjVu_root/PocketDjvu/BookmarkDlg.cpp
@@ -0,0 +1,26 @@
+// BookmarkDlg.cpp : Implementation of CBookmarkDlg
+
+#include "stdafx.h"
+#include "BookmarkDlg.h"
+
+CBookmarkDlg::CBookmarkDlg( bool bAddBookmark ) : 
+  m_bAddBookmark(bAddBookmark)
+{
+}
+
+CBookmarkDlg::~CBookmarkDlg()
+{
+}	
+
+LRESULT CBookmarkDlg::OnInitDialog( UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled )
+{
+  Base::OnInitDialog( uMsg, wParam, lParam, bHandled );
+  bHandled = true;
+  return 1;  // Let the system set the focus
+}
+
+LRESULT CBookmarkDlg::OnClickedOK( WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled )
+{
+  EndDialog( wID );
+  return 0;
+}
