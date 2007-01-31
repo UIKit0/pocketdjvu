3a2ceaef9e93b6288c9baf7b7d94271090a7ef2a
Igor.Solovyov <Igor.Solovyov@84cd470b-3125-0410-acc3-039690e87181>
2007-01-31 21:45:09 +0000

    1-st raw VM memory undebugged routines...

--- trunk/PocketDjVu_root/VMem/VMem.cpp
+++ trunk/PocketDjVu_root/VMem/VMem.cpp
@@ -18,79 +18,167 @@ extern "C"
 namespace siv
 {
 
+  static wchar_t const * g_cSwapFileName = L"\\SD Card\\file.map";
+  //---------------------------------------------------------------------------
+  class CMemInit
+  {
+  private:
+    explicit CMemInit( wchar_t const * szSwapFileName, unsigned sizeMB = 32 ) :
+      m_baseAddr()
+      , m_size(sizeMB*1024*1024)
+      , m_err()
+      , m_msp()
+    {
+      // TODO: check if exist and not create in such case
+      HANDLE hF = CreateFile( szSwapFileName, GENERIC_READ|GENERIC_WRITE,
+                            FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL );
+      if ( INVALID_HANDLE_VALUE == hF )
+      {
+        m_err = GetLastError();
+        return;
+      }
+
+      HANDLE hFM = CreateFileMapping( hF, NULL, PAGE_READWRITE, 0, m_size, L"swap" );
+      if ( !hFM )
+      {
+        m_err = GetLastError();
+        CloseHandle( hF );
+        return;
+      }
+
+      CloseHandle( hF );
+
+      m_baseAddr = MapViewOfFile( hFM, FILE_MAP_WRITE, 0, 0, m_size );
+      if ( !m_baseAddr )
+      {
+        m_err = GetLastError();
+      }
+      CloseHandle( hFM );
+      
+      m_msp = create_mspace_with_base( m_baseAddr, m_size, true );
+    }
+
+    ~CMemInit()
+    {
+      Destroy();
+    }
+
+    void Destroy()
+    {
+      if ( m_baseAddr )
+      {
+        if ( m_msp )
+        {
+          destroy_mspace( m_msp );
+          m_msp = 0;
+        }
+        UnmapViewOfFile( m_baseAddr );
+        m_baseAddr = 0;
+      }
+      m_bInited = false;
+    }
+
+  public:
+    static CMemInit * GetInstance()
+    {
+      if ( !m_bInited )
+      {
+        ATL::CCritSecLock lock( m_cs );
+        if ( !m_bInited )
+        {
+          m_instance = ::new( m_space ) CMemInit( g_cSwapFileName );
+          if ( !m_instance->IsValid() )
+          {
+            m_instance->~CMemInit();
+            m_instance = 0;
+            // TODO: Show the critical error message here.
+          }
+          m_bInited = true;
+        }
+      }
+      return m_instance;
+    }
+
+    bool IsValid()
+    {
+      return !!m_msp;
+    }
+
+    DWORD GetLastError()
+    {
+      return m_err;
+    }
+
+    operator mspace()
+    {
+      return m_msp;
+    }
+
+    void Enable( bool bEnable )
+    {
+      ::ATL::CCritSecLock lock( m_cs );
+      m_bEnable = bEnable;
+    }
+
+  // DATA:
+  private:
+    LPVOID m_baseAddr;
+    DWORD  m_size;
+    DWORD  m_err;
+    mspace m_msp;
+
+    static bool volatile            m_bEnable;
+    static bool volatile            m_bInited;
+    static ::ATL::CCriticalSection  m_cs;
+    static CMemInit *               m_instance;
+    static char                     m_space[];
+  };
+
+  __declspec(selectany) bool volatile           CMemInit::m_bEnable = true;
+  __declspec(selectany) bool volatile           CMemInit::m_bInited = false;
+  __declspec(selectany) ::ATL::CCriticalSection CMemInit::m_cs;
+  __declspec(selectany) CMemInit *              CMemInit::m_instance = 0;
+  __declspec(selectany) char                    CMemInit::m_space[ sizeof CMemInit ];
+
+  //---------------------------------------------------------------------------
   void     vm_free( void * pMem )
   {
-    free(pMem);
+    CMemInit * pmi = CMemInit::GetInstance();
+    if ( !pmi )
+    {
+      free( pMem );
+      return;
+    }
+    mspace_free( *pmi, pMem );
   }
 
   void *   vm_malloc( size_t bytes )
   {
-    return malloc(bytes);
+    CMemInit * pmi = CMemInit::GetInstance();
+    if ( !pmi )
+    {
+      return malloc( bytes );
+    }
+    return mspace_malloc( *pmi, bytes );
   }
 
   void *   vm_realloc( void * pMem, size_t bytes )
   {
-    return realloc( pMem, bytes );
+    CMemInit * pmi = CMemInit::GetInstance();
+    if ( !pmi )
+    {
+      return realloc( pMem, bytes );
+    }
+    return mspace_realloc( *pmi, pMem, bytes );
   }
 
   void *   vm_calloc( size_t n_elements, size_t elem_size )
   {
-    return calloc( n_elements, elem_size );
+    CMemInit * pmi = CMemInit::GetInstance();
+    if ( !pmi )
+    {
+      return calloc( n_elements, elem_size );
+    }
+    return mspace_calloc( *pmi, n_elements, elem_size );
   }
 } // namespace siv
-
-#if 0
-wchar_t const * fileName = L"\\SD Card\\file.map";
-
-  //CWaitCursor wc;
-  DWORD err = 0;
-
-  HANDLE hF = CreateFile( fileName, GENERIC_READ|GENERIC_WRITE,
-                          FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL );
-  if ( INVALID_HANDLE_VALUE == hF )
-  {
-    err = GetLastError();
-    wInfo.SetWindowText( L"CreateFile is failed." );
-    return;
-  }
-
-  const DWORD numBytesFM = mb * 1024*1024;
-  HANDLE hFM = CreateFileMapping( hF, NULL, PAGE_READWRITE, 0, numBytesFM, L"fileName" );
-  if ( !hFM )
-  {
-    err = GetLastError();
-    CloseHandle( hF );
-    wInfo.SetWindowText( L"CreateFileMapping is failed." );
-    return;
-  }
-
-  CloseHandle( hF );
-
-  LPVOID addr = MapViewOfFile( hFM, FILE_MAP_WRITE, 0, 0, numBytesFM );
-  CloseHandle( hFM );
-  if ( !addr )
-  {
-    err = GetLastError();
-    wInfo.SetWindowText( L"MapViewOfFile is failed." );
-    return;
-  }
-
-  unsigned i=0;
-  __try
-  {
-    BYTE * pCh = (BYTE *)addr;
-    for ( i=0;  i < numBytesFM; ++i )
-    {
-      pCh[i] = 'i';
-    }
-
-    TestDLmalloc( addr, numBytesFM );
-  }
-  __except( EXCEPTION_EXECUTE_HANDLER )
-  {
-    err = GetLastError();
-    UnmapViewOfFile( addr );
-    wInfo.SetWindowText( L"Exception!" );
-    return;
-  }
-#endif
